06b2db070685d7754c85d92192bd35b5952b1659
Kateee17 <catherinegalyametdinova27@gmail.com>
2024-11-17 16:23:59 +0500

    lesson06K

---
+++ lesson_7/test_form.py
@@ -0,0 +1,24 @@
+import pytest
+from selenium import webdriver
+from form import Form
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome()
+    yield driver
+    driver.quit()
+def test_form(driver):
+    form = Form(driver)
+    form.name("РРІР°РЅ")
+    form.surname("РџРµС‚СЂРѕРІ")
+    form.address("Р›РµРЅРёРЅР°, 55-3")
+    form.email("test@skypro.com")
+    form.phone_number("+7985899998787")
+    form.zip_code("")
+    form.city("РњРѕСЃРєРІР°")
+    form.country("Р РѕСЃСЃРёСЏ")
+    form.job_position("QA")
+    form.company("SkyPro")
+    form.knopka_submit()
+    form.danger_color()
+    form.success_color()
